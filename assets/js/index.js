d8f8c5aceb9ac4d18c11bb491d88ff33bea6ea5a
WLDC <17862883453.com>
2020-06-21 20:00:00 +0800

    基本完成登录页，首页头像，右侧首页显示，退出

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,57 +1,114 @@
 $(function () {
-  $('.text-avatar').hide()
-  $('.layui-nav-img').hide()
+  $('.layui-nav-img').hide();
+  $('.text-avatar').hide();
+
   getUserInfo();
 
-  // 获取弹出层对象
+  //退出功能
+  //1. 根据layui 获取弹出对象
   var layer = layui.layer;
-
-  // 点击按钮，实现退出功能
-  $('#btnLogout').on('click', function() {
-    // 提示用户是否确认退出
-    layer.confirm('确定退出登录?', { icon: 3, title: '提示' }, function(index) {
-      //do something
-      // 1. 清空本地存储中的 token
-      localStorage.removeItem('token')
-      // 2. 重新跳转到登录页面
-      location.href = '/login.html'
-
-      // 关闭 confirm 询问框
-      layer.close(index)
-    })
-  })
-})
-
-// 获取用户的基本信息
+  //2. 点击退出 返回登录页面
+  $('#outBack').on('click',function(){
+    //3. 弹出确认框
+    layer.confirm('你确定要退出吗?', {icon: 3, title:'提示'}, function(index){
+      //点击确定 
+      //3.1 清空本地储存
+      localStorage.removeItem('token');
+      //3.2 跳转登录页面
+      location.href = '/big_project/login.html';
+      
+      layer.close(index);
+    });  
+  });
+});
 function getUserInfo() {
+  //获取用户基本信息
   $.ajax({
     method: 'GET',
     url: '/my/userinfo',
-    // headers 就是请求头配置对象
     headers: {
-      Authorization: localStorage.getItem('token') || ''
+      Authorization: localStorage.getItem('token') || '',
     },
-    success: function(res) {
+    success: function (res) {
+      //  console.log(res);
       if (res.status !== 0) {
         return layui.layer.msg('获取用户信息失败！')
       }
       // 调用 renderAvatar 渲染用户的头像
+      // data   : 用户的基本信息
       renderAvatar(res.data)
-    },
-    // // 不论成功还是失败，最终都会调用 complete 回调函数
-    // complete: function(res) {
-    //   // console.log('执行了 complete 回调：')
-    //   // console.log(res)
-    //   // 在 complete 回调函数中，可以使用 res.responseJSON 拿到服务器响应回来的数据
-    //   if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
-    //     // 1. 强制清空 token
-    //     localStorage.removeItem('token')
-    //     // 2. 强制跳转到登录页面
-    //     location.href = '/login.html'
-    //   }
-    // }
+    }
   })
+};
+
+//渲染用户头像  传递形参
+function renderAvatar (user) {
+  //1. 获取用户的名称
+  var name = user.nickname || user.username;
+  // 2. 设置欢迎的文本
+  $('#welcome').html('欢迎&nbsp;&nbsp;' + name);
+  // 3. 按需求 渲染用户的头像  判断 是否为图片 如果不是图片 使用文本头像
+        if(user.user_pic !== null) {
+          // 3.1 渲染图片头像   隐藏文本头像
+          $('.layui-nav-img').attr('src',user.user_pic).shouw();
+          //隐藏文本头像
+          $('.text-avatar').hide();
+        }else {
+          // 3.2 渲染文本头像   隐藏图片头像
+          //获取文本  //首个文本改为大写
+          $('.text-avatar').html(name[0].toUpperCase()).show();
+          $('.layui-nav-img').hide();
+          
+        }
+        
+        
+
+  
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // 获取用户的基本信息
+// function getUserInfo() {
+//   $.ajax({
+//     method: 'GET',
+//     url: '/my/userinfo',
+//     // headers 就是请求头配置对象
+//     headers: {
+//       Authorization: localStorage.getItem('token') || ''
+//     },
+//     success: function(res) {
+//       if (res.status !== 0) {
+//         return layui.layer.msg('获取用户信息失败！')
+//       }
+//       // 调用 renderAvatar 渲染用户的头像
+//       renderAvatar(res.data)
+//     },
+//     // // 不论成功还是失败，最终都会调用 complete 回调函数
+//     // complete: function(res) {
+//     //   // console.log('执行了 complete 回调：')
+//     //   // console.log(res)
+//     //   // 在 complete 回调函数中，可以使用 res.responseJSON 拿到服务器响应回来的数据
+//     //   if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+//     //     // 1. 强制清空 token
+//     //     localStorage.removeItem('token')
+//     //     // 2. 强制跳转到登录页面
+//     //     location.href = '/login.html'
+//     //   }
+//     // }
+//   })
+// }
 
 // 渲染用户的头像
 function renderAvatar(user) {
@@ -76,12 +133,12 @@ function renderAvatar(user) {
   }
 }
 
-  /**
-   * 主动切换 nav 
-   * @param {*} newNavId 新的 nav 元素 id
-   * @param {*} oldNavId 旧的 nav 元素 id
-   */
-  function changeNav (newNavId, oldNavId) {
-    $('#' + oldNavId).removeClass('layui-this');
-    $('#' + newNavId).addClass('layui-this');
-  }
+/**
+ * 主动切换 nav 
+ * @param {*} newNavId 新的 nav 元素 id
+ * @param {*} oldNavId 旧的 nav 元素 id
+ */
+function changeNav(newNavId, oldNavId) {
+  $('#' + oldNavId).removeClass('layui-this');
+  $('#' + newNavId).addClass('layui-this');
+}
--- assets/js/login.js
+++ assets/js/login.js
@@ -1,11 +1,116 @@
 $(function(){
   //登录页面 点击切换注册
-  $('#register').on('click',function(){
+  $('.links #register').on('click',function(){
     $('.login_box').hide();
     $('.register_box').show();
   });
-  $('#login').on('click',function(){
+  $('.links #login').on('click',function(){
     $('.login_box').show();
     $('.register_box').hide();
   });
+
+    var form = layui.form;
+    var layer = layui.layer;
+    form.verify({
+      // 密码校验
+      pwd : [
+        /^[\S]{6,12}$/
+        ,'密码必须6到12位，且不能出现空格'
+      ],
+       repwd : function(value){
+         var pwd = $('.register_box #password').val();
+         //判断
+         if(pwd !== value){
+           return '两次输入密码不一致';
+         }
+       }
+    });
+
+     // 监听注册表单的提交事件
+  $('#reg_form').on('submit', function(e) {
+    // 1. 阻止默认的提交行为
+    e.preventDefault()
+    // 2. 发起Ajax的POST请求
+    var data = {
+      username: $('#reg_form [name=username]').val(),
+      password: $('#reg_form [name=password]').val()
+    }
+    $.post('/api/reguser', data, function(res) {
+      if (res.status !== 0) {
+        return layer.msg(res.message)
+      }
+      layer.msg('注册成功，请登录！')
+      // 模拟人的点击行为
+      $('#login').click()
+  })
+})
+
+// 监听登录表单的提交事件
+$('#login_form').submit(function(e) {
+  // 1.阻止默认提交行为
+  e.preventDefault()
+  // 2. 发起Ajax的POST请求
+  $.ajax({
+    url: '/api/login',
+    method: 'POST',
+    // 快速获取表单中的数据
+    data: $(this).serialize(),
+    success: function(res) {
+      if (res.status !== 0) {
+        return layer.msg('登录失败！')
+      }
+      layer.msg('登录成功！')
+      // 将登录成功得到的 token 字符串，保存到 localStorage 中
+      localStorage.setItem('token', res.token)
+      // 跳转到后台主页
+      // location.href = '/index.html';
+      location.href = '/big_project/index.html'
+    }
+  })
+})
+
+
+
+
+
+
+    // // 监听注册
+    // $('#reg_form').on('submit',function(e){
+    //   e.preventDefault();
+    //   //获取数据
+    //   var data = {
+    //     username : $('#reg_form [name = "username"]').val(),
+    //     password : $('#reg_form [name = "username"]').val()
+    //   };
+    //   //post请求
+    //   $.post('/api/reguser',data,function(res){
+    //     console.log(res);
+    //     if(res.status !== 0){
+    //       return layer.msg(res.message);
+    //     };
+    //     layer.msg('注册成功,请登录');
+    //     //成功后跳转登录页面
+    //     $('#login').click();
+    //   })
+    // });
+
+    //  // 监听登录
+
+    //   $('#login_form').on('submit',function(e){
+    //    e.preventDefault();
+    //    //获取数据
+    //    var data =$(this).serialize();
+    //    //post请求
+    //    $.post('http://ajax.frontend.itheima.net/api/login',data,function(res){
+    //      console.log(res);
+    //      if(status !== 0){
+    //        return layer.msg('登录失败');
+    //      };
+    //      layer.msg('登录成功');
+    
+    //      //
+    //      localStorage.setItem('token', res.token);
+    //      location.href = "/big_project/index.html";
+    //    })
+    //  });
 })
