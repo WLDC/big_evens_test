f01f558dde5a567ae9230ebc777202238e427c41
WLDC <17862883453.com>
2020-06-23 20:06:19 +0800

    完成了文章类别增加，修改，删除

--- assets/js/article/art_cate.js
+++ assets/js/article/art_cate.js
@@ -1,15 +1,16 @@
-$(function(){
+$(function () {
 
-  var layer =layui.layer;
+  var layer = layui.layer;
+  var form = layui.form;
 
 
   getCateList();
 
-  function getCateList(){
+  function getCateList() {
     $.ajax({
-      method : 'GET',
-      url : '/my/article/cates',
-      success : function(res){
+      method: 'GET',
+      url: '/my/article/cates',
+      success: function (res) {
         console.log(res);
         var htmlStr = template('art_cate', res);
         $('tbody').html(htmlStr)
@@ -18,42 +19,106 @@ $(function(){
   }
 
   //弹出增加表单
-   
-   $('#cateAdd').on('click',function(){
+  var indexAdd =null;
+  $('#cateAdd').on('click', function () {
     indexAdd = layer.open({//
-    type: 1,
-    area: ['500px', '250px'],
-    title: '添加文章分类',
-    content: $('#dialog-add').html()
-  })
-});
+      type: 1,
+      area: ['500px', '250px'],
+      title: '添加文章分类',
+      content: $('#dialog-add').html()
+    })
+  });
 
 
   //点击确认添加
-  $('body').on('submit','#form-add',function(e){
+  $('body').on('submit', '#form-add', function (e) {
     e.preventDefault();
     $.ajax({
-      method : 'post',
-      url : '/my/article/addcates',
-      data : $(this).serialize(),
-      success : function(res){
+      method: 'post',
+      url: '/my/article/addcates',
+      data: $(this).serialize(),
+      success: function (res) {
         console.log(res);
-        if(res.status !== 0) {
+        if (res.status !== 0) {
           return layer.msg('添加失败');
         }
-        
+
         getCateList();
         layer.msg('成功');
         layer.close(indexAdd);
       }
     })
   })
-//////
+  //////
   //修改
+  // 1.点击修改 弹出页面
+  //代理修改， 因为是后来添加的内容
+  var indexEdit = null;
+  $('tbody').on('click', '.btn-edit', function () {
+    indexEdit = layer.open({
+      type: 1,
+      area: ['500px', '250px'],
+      title: '修改文章分类',
+      content: $('#dialog-edit').html()
+    })
+    //根据id 获取要修改的数据
+    //获取id
+    var id = $(this).attr('data-id');
+    $.ajax({
+      method: 'get',
+      url: '/my/article/cates/' + id,
+      success: function (res) {
+        // console.log(res);
+        form.val('form-edit', res.data);
+      }
 
+    })
+  })
 
+  // 2. 点击确认修改 刷新列表页面
+  //事件代理 确认修改
+
+  $('body').on('submit', '#form-edit', function (e) {
+    e.preventDefault();
+    $.ajax({
+      method: 'post',
+      url: '/my/article/updatecate',
+      //获取输入的数据
+      data : $(this).serialize(),
+      success: function (res) {
+        if (res.status !== 0) {
+          return layer.msg('修改文章列表失败');
+        }
+        layer.msg('修改文章列表成功');
+        layer.close(indexEdit);
+        getCateList();
+      }
+    });
+  });
 
   //删除
+  // 点击删除  代理  获取id  
+  $('tbody').on('click', '.btn-delete', function (e) {
+    e.preventDefault();
+    //获取id
+    var id = $(this).attr('data-id');
+    //弹出 询问框 是否删除
+    layer.confirm('确定要删除吗?', { icon: 3, title: '提示' }, function (index) {
+      $.ajax({
+        method: 'get',
+        url: '/my/article/deletecate/' + id,
+        success: function (res) {
+          if (res.status !== 0) {
+            return layer.msg('删除失败');
+          }
+          layer.msg('删除成功');
+          layer.close(index);
+          getCateList();
+        }
+      })
+    });
+  })
+
 })
 
 
