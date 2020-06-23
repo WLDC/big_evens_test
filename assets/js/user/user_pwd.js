12b0bcb2fb9446b3315244e05f4de69ca687c624
WLDC <17862883453.com>
2020-06-23 14:42:24 +0800

    完成个人中心，及文章的静态页面

--- assets/js/user/user_pwd.js
+++ assets/js/user/user_pwd.js
@@ -1,34 +1,103 @@
-$(function () {
-  var form = layui.form;
+$(function(){
+  var form = layui.form
 
   form.verify({
     pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
-    samePwd: function (value) {
+    samePwd: function(value) {
       if (value === $('[name=oldPwd]').val()) {
         return '新旧密码不能相同！'
       }
     },
-    rePwd: function (value) {
+    rePwd: function(value) {
       if (value !== $('[name=newPwd]').val()) {
         return '两次密码不一致！'
       }
-    } 
-  })
+    }
+  });
 
-  $('.layui-form').on('submit', function(e) {
-    e.preventDefault()
+  $('#restBtn').on('submit',function(e){
+    e.preventDefault();
+    // restMima();
     $.ajax({
-      method: 'POST',
-      url: '/my/updatepwd',
-      data: $(this).serialize(),
-      success: function(res) {
-        if (res.status !== 0) {
-          return layui.layer.msg('更新密码失败！')
+      method : 'post',
+      url : '/my/updatepwd',
+      data : $(this).serialize(),
+      success : function(res){
+        if(res.status !== 0){
+          return layui.layer.msg('更新密码失败！');
         }
         layui.layer.msg('更新密码成功！')
         // 重置表单
-        $('.layui-form')[0].reset()
+        $('#restBtn')[0].reset()
       }
     })
   })
+
+
+  function restMima(){
+    $.ajax({
+      method : 'post',
+      url : '/my/updatepwd',
+      data : $(this).serialize(),
+      success : function(res){
+        if(res.status !== 0){
+          return layui.layer.msg('更新密码失败！');
+        }
+        layui.layer.msg('更新密码成功！')
+        // 重置表单
+        $('#restBtn')[0].reset()
+      }
+    })
+  }
+
+
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $(function () {
+//   var form = layui.form;
+
+//   form.verify({
+//     pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
+//     samePwd: function (value) {
+//       if (value === $('[name=oldPwd]').val()) {
+//         return '新旧密码不能相同！'
+//       }
+//     },
+//     rePwd: function (value) {
+//       if (value !== $('[name=newPwd]').val()) {
+//         return '两次密码不一致！'
+//       }
+//     } 
+//   })
+
+//   $('.layui-form').on('submit', function(e) {
+//     e.preventDefault()
+//     $.ajax({
+//       method: 'POST',
+//       url: '/my/updatepwd',
+//       data: $(this).serialize(),
+//       success: function(res) {
+//         if (res.status !== 0) {
+//           return layui.layer.msg('更新密码失败！')
+//         }
+//         layui.layer.msg('更新密码成功！')
+//         // 重置表单
+//         $('.layui-form')[0].reset()
+//       }
+//     })
+//   })
+// })
